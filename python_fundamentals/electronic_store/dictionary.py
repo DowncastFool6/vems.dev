ec124bdae16fa13083d0205f5db0d6181dd81549
Vissolela Cundi <vems.dev@proton.me>
2026-01-13 19:02:16 +0000

    Review

---
+++ python_fundamentals/electronic_store/dictionary.py
@@ -0,0 +1,25 @@
+electronic_stock = {
+    "smartphones": {
+        "brand": "Apple",
+        "models": [
+            {"name": "iPhone 14 Pro", "price": 1200, "quantity": 25},
+            {"name": "iPhone 14", "price": 800, "quantity": 50},
+            {"name": "iPhone SE", "price": 450, "quantity": 15}
+        ]
+    },
+    "laptops": {
+        "brand": "Dell",
+        "models": [
+            {"name": "XPS 15", "price": 2000, "quantity": 10},
+            {"name": "Inspiron 14", "price": 750, "quantity": 30}
+        ]
+    },
+    "accessories": {
+        "brand": "Diverse",
+        "models": [
+            {"name": "Wireless Headphones", "price": 150, "quantity": 100},
+            {"name": "Smartphone Cover", "price": 25, "quantity": 200},
+            {"name": "USB-C Charger", "price": 30, "quantity": 150}
+        ]
+    }
+}
---
+++ python_fundamentals/hangman/hangman_words.py
@@ -0,0 +1,1 @@
+word_list = ["python", "keyboard", "hangman", "mouse", "ide"]
